48c7a35b38511bdf0e0c9bd3d7f58d5bf9f1ec12
Jaiderin123 <torrezjaider10@gmail.com>
2026-04-13 18:39:54 -0500

    First commit

---
+++ src/main/java/com/accenture/assessment/AccentureSkillTestApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureSkillTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccentureSkillTestApplication.class, args);
+    }
+
+}
